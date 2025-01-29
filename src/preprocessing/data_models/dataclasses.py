97cb7dd9b728141efa74cf631053ef75503bdc48
Tobi <ulkmv@student.kit.edu>
2025-01-29 14:49:17 +0100

    add is_anomaly to dataclass

--- src/preprocessing/data_models/dataclasses.py
+++ src/preprocessing/data_models/dataclasses.py
@@ -16,28 +16,30 @@ class Event:
     event_end: datetime
     event_end_id: int
     event_description: Optional[str]
+    is_anomaly: bool
 
     @classmethod
     def from_json(cls, event_id: int) -> "Event":
         wind_farm = Helper.wind_farm_for_dataset(dataset_id=event_id)
-        event_base_json_path = os.getenv("EVENT_BASE_PATH_JSON")
+        event_base_json_path = os.getenv("EVENTS_BASE_JSON_PATH")
         event_json_path = event_base_json_path.format(wind_farm=wind_farm)
 
         with open(event_json_path, "r") as event_file:
             data = json.load(event_file)
 
-        event_data = data.get(event_id)
+        event_data = data.get(str(event_id))
         if not event_data:
             raise ValueError(f"Event ID {event_id} not found in metadata for wind farm {wind_farm}.")
 
         return cls(
-            event_id=event_data["event_id"],
+            event_id=event_id,
             event_label=event_data["event_label"],
             event_start=event_data["event_start"],
-            event_start_id=event_data["event_start_id"],
+            event_start_id=int(event_data["event_start_id"]),
             event_end=event_data["event_end"],
-            event_end_id=event_data["event_end_id"],
-            event_description=event_data.get("event_description")
+            event_end_id=int(event_data["event_end_id"]),
+            event_description=event_data.get("event_description"),
+            is_anomaly=event_data["is_anomaly"]
         )
 
 
@@ -53,7 +55,7 @@ class Sensor:
     @classmethod
     def from_json(cls, sensor_name: str, event_id: int) -> "Sensor":
         wind_farm = Helper.wind_farm_for_dataset(dataset_id=event_id)
-        sensor_base_json_path = os.getenv("SENSOR_BASE_PATH_JSON")
+        sensor_base_json_path = os.getenv("SENSORS_BASE_JSON_PATH")
         sensor_json_path = sensor_base_json_path.format(wind_farm=wind_farm)
 
         with open(sensor_json_path, "r", encoding="utf-8") as sensor_file:
@@ -61,7 +63,7 @@ class Sensor:
 
         sensor_data = data.get(sensor_name)
         if not sensor_data:
-            raise KeyError(f"Sensor '{sensor_name}' not found in JSON file.")
+            sensor_data = data.get(sensor_name + "_avg")
 
         return cls(
             name=sensor_data["name"],
@@ -69,5 +71,5 @@ class Sensor:
             stat_type=sensor_data["stat_type"],
             unit=sensor_data["unit"],
             is_angle=sensor_data["is_angle"],
-            is_counter=sensor_data["is_counter"],
+            is_counter=sensor_data["is_counter"]
         )
